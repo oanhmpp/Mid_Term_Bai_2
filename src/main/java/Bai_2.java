63ca942febb81820557013be9f9b283ec21be1af
oanhmpp <oanhmpp@gmail.com>
2021-08-31 09:36:24 +0700

    Initial commit

---
+++ src/main/java/Bai_2.java
@@ -0,0 +1,22 @@
+public class Bai_2 {
+    public static String checkPassword(String s) {
+        String re = "";
+        String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{10,}$";
+        String regex2 = "[!@#$%^&*()_+=-]";
+        char[] arrS = s.toCharArray();
+
+        if (s.matches(regex)) {
+            re = "Strong password";
+        } else if (s.length() < 10) {
+            re = "Weak password !!!";
+        } else if (!s.matches(regex2) && s.length() >= 10) {
+            re = "Password is not strong enough";
+        }
+        return re;
+    }
+
+    public static void main(String[] args) {
+        String s = "Oanhnguyeh@1";
+        System.out.println(checkPassword(s));
+    }
+}
